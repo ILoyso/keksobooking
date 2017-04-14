fdc6fe88f452725d5595a10b6e187a25a60edef2
Irina <loyrina@gmail.com>
2017-04-15 00:05:33 +0400

    form validate

--- js/map.js
+++ js/map.js
@@ -222,3 +222,107 @@ descriptionClose.addEventListener('keydown', function (evt) {
     removeClass(allMarks, 'pin--active');
   }
 });
+
+
+
+
+
+
+// form validate
+
+var formWrap = document.querySelector('.notice__form');
+var formTitle = formWrap.querySelector('#title');
+var formPrice = formWrap.querySelector('#price');
+var formType = formWrap.querySelector('#type');
+var formTimeIn = formWrap.querySelector('#time');
+var formTimeOut = formWrap.querySelector('#timeout');
+var formRooms = formWrap.querySelector('#room_number');
+var formCapacity = formWrap.querySelector('#capacity');
+var formSubmit = formWrap.querySelector('.form__submit');
+
+
+var validateTitle = function() {
+  var value = formTitle.value;
+  if ((value.length < 30) || (value.length > 100)) {
+    formTitle.setAttribute('style', 'border: 1px solid red;');
+    return;
+  }
+  formTitle.setAttribute('style', 'border: 1px solid #d9d9d3;');
+}
+
+var validatePrice = function() {
+  var value = formPrice.value;
+  if ((value < 0) || (value > 1000000) || ' ') {
+    formPrice.setAttribute('style', 'border: 1px solid red;');
+    return;
+  }
+  formPrice.setAttribute('style', 'border: 1px solid #d9d9d3;');
+}
+
+var validatePriceByType = function(typeValue) {
+  switch (typeValue) {
+    case ('Лачуга'):
+      formPrice.value = 0;
+      break;
+    case ('Квартира'):
+      formPrice.value = 1000;
+      break;
+    case ('Дворец'):
+      formPrice.value = 10000;
+      break;
+  }
+}
+
+var validateType = function() {
+  var typeValue = formType.value;
+  validatePriceByType(typeValue);
+}
+
+var validateTime = function() {
+  var timeInIndex = formTimeIn.selectedIndex;
+  formTimeOut.selectedIndex = timeInIndex;
+}
+
+var validateCapacityByRooms = function() {
+  var value = formRooms.value;
+  if (value <= 2) {
+    formCapacity.selectedIndex = 1;
+  } else {
+    formCapacity.selectedIndex = 0;
+  }
+}
+
+var validateForm = function(evt) {
+  var formFieldId = evt.target;
+
+  switch (formFieldId) {
+    case (formTitle):
+      validateTitle();
+      break;
+    case (formPrice):
+      validatePrice();
+      break;
+    case (formType):
+      validateType();
+      break;
+    case (formTimeIn):
+      validateTime();
+      break;
+    case (formRooms):
+      validateCapacityByRooms();
+      break;
+  }
+}
+
+var submitingForm = function(evt) {
+  validateTitle();
+  validatePrice();
+}
+
+formWrap.addEventListener('change', function(evt){
+  validateForm(evt);
+})
+
+formSubmit.addEventListener('click', function(){
+  submitingForm();
+})
